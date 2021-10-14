6554f32c76681815a5510b243d4a9016b374c1a1
Alexander Tsoy <me@alexandertsoy.com>
2021-10-14 10:44:47 +0500

    Добавил навигацию.

--- lib/views/description_page.dart
+++ lib/views/description_page.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:market_cap_chart/models/overview.dart';
 
 /// Экран с подробной информацией о компании.
-class DescriptionPage extends StatelessWidget
-{
+class DescriptionPage extends StatelessWidget {
   const DescriptionPage({Key? key}) : super(key: key);
 
   static String routeName = '/description';
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    final overview = ModalRoute.of(context)!.settings.arguments as Overview;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Описание компании'),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text(overview.name),
+          ),
+          ListTile(
+            title: Text(overview.marketCapitalization),
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,7 +1,7 @@
-
 import 'package:flutter/material.dart';
 import 'package:market_cap_chart/models/overview.dart';
 import 'package:market_cap_chart/services/market_cap_service.dart';
+import 'package:market_cap_chart/views/description_page.dart';
 
 /// Главный экран.
 class HomePage extends StatefulWidget {
@@ -19,29 +19,41 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: StreamBuilder(
-        stream: widget.marketCapService.getOverviewStream(),
-        builder: (context, snapshot) {
-          var caption = '';
-          if (snapshot.hasData)
-          {
-            caption = (snapshot.data as Overview).name;
-            overviews.add(snapshot.data as Overview);
-          }
+      appBar: AppBar(
+        title: const Text('Список компаний'),
+      ),
+      body: Center(
+        child: StreamBuilder(
+            stream: widget.marketCapService.getOverviewStream(),
+            builder: (context, snapshot) {
+              var caption = '';
+              if (snapshot.hasData) {
+                caption = (snapshot.data as Overview).name;
+                overviews.add(snapshot.data as Overview);
+              }
 
-          if (snapshot.connectionState == ConnectionState.done)
-          {
-            List<Widget> tiles = [];
-            for (final overview in overviews)
-              //TODO: Добавить навигацию к описанию компании.
-              tiles.add(ListTile(title: Text(overview.name),));
-            return ListView(children: tiles);
-          }
-          else
-          {
-            return Column(mainAxisAlignment: MainAxisAlignment.center, children: [CircularProgressIndicator(), Text(caption)]);
-          }
-        }),),
+              if (snapshot.connectionState == ConnectionState.done) {
+                List<Widget> tiles = [];
+                for (final overview in overviews) {
+                  //TODO: Поменять на Chart.
+                  tiles.add(ListTile(
+                    title: Text(overview.name),
+                    onTap: () => Navigator.of(context).pushNamed(
+                        DescriptionPage.routeName,
+                        arguments: overview),
+                  ));
+                }
+                return ListView(children: tiles);
+              } else {
+                return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const CircularProgressIndicator(),
+                      Text(caption),
+                    ]);
+              }
+            }),
+      ),
     );
   }
 }
